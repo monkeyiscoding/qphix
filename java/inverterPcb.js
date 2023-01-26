3a8795dc78b2dfef8fe0e603a1b460f850e82868
monkeyiscoding <109468448+monkeyiscoding@users.noreply.github.com>
2023-01-26 14:13:02 +0530

    asasdasd

--- java/inverterPcb.js
+++ java/inverterPcb.js
@@ -9,6 +9,7 @@ const firebaseConfig = {
   measurementId: "G-KMZM22KYZW"
 };
 
+
 firebase.initializeApp(firebaseConfig);
 var query = firebase.database().ref("training_months");
 
@@ -81,16 +82,21 @@ $("#close-booking").click(function() {
   $("#dialog-booking").fadeOut();
 })
 
+var month = "";
 function fillForm(text) {
+
+  month = text;
   $("#title-text").html(`REGISTER YOUR SEAT FOR ${text}`)
   $("#dialog-booking").fadeIn();
+
 }
 
 
 
 
 
-
+var ad = false;
+var sel = false;
 const image_input = document.querySelector("#image-input");
 
 image_input.addEventListener("change", function() {
@@ -103,6 +109,7 @@ image_input.addEventListener("change", function() {
 
     const uploaded_image = reader.result;
     document.querySelector("#display-image").style.backgroundImage = `url(${uploaded_image})`;
+    sel = true;
   });
   reader.readAsDataURL(this.files[0]);
 });
@@ -120,6 +127,7 @@ image_input_aadhar.addEventListener("change", function() {
 
     const uploaded_image = reader.result;
     document.querySelector("#display-image-aadhar").style.backgroundImage = `url(${uploaded_image})`;
+    ad = true;
   });
   reader.readAsDataURL(this.files[0]);
 });
@@ -128,47 +136,192 @@ image_input_aadhar.addEventListener("change", function() {
 
 
 
-
-
-
-
 // razor PAY
 
-var options = {
-  "key": "rzp_live_25RshOlv3jiE3w", // Enter the Key ID generated from the Dashboard
-  "amount": "300000", // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
-  "currency": "INR",
-  "name": "Qphix",
-  "description": "Training Registration",
-  "image": "image/logo_black.png",
-  "callback_url": "https://eneqd3r9zrjok.x.pipedream.net/",
-  "handler": function(response) {
-    if (typeof response.razorpay_payment_id == 'undefined' || response.razorpay_payment_id < 1) {
-      alert("Failed");
-    } else {
-      alert("Thanks");
-    }
-    location.href = redirect_url;
-  },
-
-  "prefill": {
-    "name": "Gaurav Kumar",
-    "email": "gaurav.kumar@example.com",
-    "contact": "9999999999"
-  },
-
-  "notes": {
-    "address": "Jaipur Rajasthan"
-  },
-  "theme": {
-    "color": "#FF6F00"
-  }
-};
 
 
 
-var rzp1 = new Razorpay(options);
+
+
+
 document.getElementById('register-seat').onclick = function(e) {
-  rzp1.open();
-  e.preventDefault();
+  var name = $("#username").val();
+  var phone = $("#phone").val();
+  var email = $("#email").val();
+  var address = $("#address").val();
+  var error = $("#error");
+
+  if (!ad) {
+    $("#error").html("Upload your aadhar card image");
+    $("#error").css("display", "block");
+  } else if (!sel) {
+    $("#error").html("Upload your selfi image");
+    $("#error").css("display", "block");
+  } else if (name.length < 4) {
+    $("#error").html("Enter your full name");
+    $("#error").css("display", "block");
+  } else if (phone.length < 10) {
+    $("#error").html("Invalid phone number");
+    $("#error").css("display", "block");
+  } else if (!email.endsWith("@gmail.com")) {
+    $("#error").html("Invalid email address");
+    $("#error").css("display", "block");
+  } else if (address.length < 8) {
+    $("#error").html("Enter complete address");
+    $("#error").css("display", "block");
+  } else {
+
+    $("#error").css("display", "none");
+
+    var options = {
+      //"key": "rzp_live_25RshOlv3jiE3w", // Enter the Key ID generated from the Dashboard
+      "key": "rzp_test_NSMMzlfJxi1ZzK", // Enter the Key ID generated from the Dashboard
+
+      "amount": "300", // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
+      "currency": "INR",
+      "name": "Qphix",
+      "description": "Training Registration",
+      "image": "image/logo_black.png",
+      "callback_url": "https://eneqd3r9zrjok.x.pipedream.net/",
+      "handler": function(response) {
+        if (typeof response.razorpay_payment_id == 'undefined' || response.razorpay_payment_id < 1) {
+          $("#loader").fadeOut();
+        } else {
+          $("#loader").fadeIn();
+          saveData()
+
+        }
+        //location.href = redirect_url;
+      },
+
+      "prefill": {
+        "name": name,
+        "email": email,
+        "contact": phone,
+      },
+
+      "notes": {
+        "address": address
+      },
+      "theme": {
+        "color": "#FF6F00"
+      }
+    };
+
+    var rzp1 = new Razorpay(options);
+    rzp1.open();
+
+    e.preventDefault();
+
+
+
+
+  }
+
+}
+
+
+
+// rzp1.on('payment.success', function (response){
+//         alert(response.error.code);
+//         alert(response.error.description);
+//         alert(response.error.source);
+//         alert(response.error.step);
+//         alert(response.error.reason);
+//         alert(response.error.metadata.order_id);
+//         alert(response.error.metadata.payment_id);
+// });
+
+
+function saveData() {
+  var name = $("#username").val();
+  var phone = $("#phone").val();
+  var email = $("#email").val();
+  var address = $("#address").val();
+
+
+  var myRef = firebase.database().ref().push();
+  var key = myRef.key;
+
+  firebase.database().ref("Registrations/" + key).set({
+    username: name,
+    phone: phone,
+    email: email,
+    address: address,
+
+  }, function(error) {
+    if (error) {
+      alert("Something went wrong try again");
+    } else {
+      uploadimage(key)
+    }
+  })
+
+}
+
+
+function uploadimage(key) {
+
+  var name = $("#username").val();
+  var phone = $("#phone").val();
+  var email = $("#email").val();
+  var address = $("#address").val();
+  //var type = getInputVal('types');
+  var storage = firebase.storage();
+  var myRef = firebase.database().ref().push();
+
+  var key2 = myRef.key;
+  var key3 = myRef.key;
+
+
+  var file = document.getElementById("image-input-aadhar").files[0];
+  var file2 = document.getElementById("image-input").files[0];
+  var storageref = storage.ref();
+  var thisref = storageref.child("registration").child(key2).put(file);
+  var thisref2 = storageref.child("registration").child(key3).put(file2);
+  thisref2.on('state_changed', function(snapshot) {
+
+
+  }, function(error) {
+
+  }, function() {
+
+
+
+    // Uploaded completed successfully, now we can get the download URL
+    thisref.snapshot.ref.getDownloadURL().then(function(downloadURL) {
+      //getting url of image
+      var aadharUrl = downloadURL;
+      thisref2.snapshot.ref.getDownloadURL().then(function(downloadURL) {
+        //getting url of image
+        var myRef = firebase.database().ref().push();
+
+
+
+        firebase.database().ref("Registrations/" + key).set({
+          aadhar: aadharUrl,
+          selfie: downloadURL,
+          username: name,
+          phone: phone,
+          email: email,
+          address: address,
+          month: month,
+
+        }, function(error) {
+          if (error) {
+
+          } else {
+            $("#loader").fadeOut();
+            $("#dialog-booking").fadeOut();
+            $("#dialog").fadeOut();
+          }
+        })
+
+
+      });
+    });
+
+
+  });
+
 }
