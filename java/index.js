d1620190dcd7d50b4d7deab8b2eae459849ffd31
monkeyiscoding <109468448+monkeyiscoding@users.noreply.github.com>
2023-01-28 18:20:43 +0530

    ze5tahtharthtaerherth

--- java/index.js
+++ java/index.js
@@ -1,11 +1,15 @@
 
-alert("okkkk");
 var login = localStorage.getItem("login");
-
 if(login == "true"){
   var name = localStorage.getItem("username");
+  var number = localStorage.getItem("number");
   $("#login-text").html(name);
-  alert(name);
+  $("#login-text-pc").html(name);
+  $("#username").html(name);
+  $("#number").html(number);
+
+  $("#login-a").css("display","none");
+  $("#login-div-after").css("display","block");
 }
 
 
